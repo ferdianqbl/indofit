f266ad009a06de2512df5aaf3e869c7487654d38
samuelsih <samuel.205025@mhs.its.ac.id>
2022-11-28 02:50:54 +0700

    prep: authentication & permission

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone_number');
             $table->string('password');
+            $table->integer('role')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
