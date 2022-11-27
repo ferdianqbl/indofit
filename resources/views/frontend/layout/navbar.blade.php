391fb9aa988a499defd2aaa936c75e6865d11d1b
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-11-27 18:28:01 +0700

    edit navbar

--- resources/views/frontend/layout/navbar.blade.php
+++ resources/views/frontend/layout/navbar.blade.php
@@ -6,7 +6,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav mx-lg-0 ms-lg-auto ms-lg-0 mx-auto text-center mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="#">TRAINER</a>
         </li>
@@ -17,7 +17,7 @@
           <a class="nav-link" href="#">ABOUT</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link login-btn" href="#">LOGIN</a>
+          <a class="nav-link login-btn d-inline-block d-lg-block" href="#">LOGIN</a>
         </li>
       </ul>
     </div>
