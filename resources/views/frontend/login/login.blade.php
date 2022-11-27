3b38caadefdf0cd71e535ded7f7f180dc57bdf90
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-11-27 18:45:53 +0700

    create login page

--- resources/views/frontend/login/login.blade.php
+++ resources/views/frontend/login/login.blade.php
@@ -1,4 +1,30 @@
 @extends('frontend.layout.main')
 
 @section('content')
+  <section class="login" id="login">
+    <div class="container">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-lg-4 col-md-6 col-8">
+          <form method="PUT" action="">
+            @csrf
+            <h1 class="form-title text-center mb-3">
+              WELCOME!
+              <span>Let's Login.</span>
+            </h1>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" id="email" name="email" placeholder="email">
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password" name="password" placeholder="password">
+            </div>
+            <div class="mt-5 text-center">
+              <button type="submit" class="btn btn-dark px-4 py-2">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
 @endsection
