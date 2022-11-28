2d14e42417e75e13ed687623bcef8a676e8320e5
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-11-28 10:52:55 +0700

    create hero

--- resources/views/frontend/user/home/index.blade.php
+++ resources/views/frontend/user/home/index.blade.php
@@ -4,7 +4,7 @@
   <div class="home" id="home">
     <section class="hero" id="hero">
       <div class="container">
-        <div class="row justify-content-center align-items-center">
+        <div class="row justify-content-center align-items-center flex-column-reverse flex-md-row">
           <div class="col-md-6 col-lg-5">
             <h1 class="hero-title">
               HIRE<br>
@@ -17,7 +17,7 @@
             </p>
             <a href="#" class="btn my-btn-primary">EXPLORE NOW</a>
           </div>
-          <div class="col-md-6 col-lg-6">
+          <div class="col-md-6 col-lg-6 mb-5 mb-md-0">
             <img src="/images/landingPage/home/hero.png" alt="hero-img" class="img-fluid">
           </div>
         </div>
