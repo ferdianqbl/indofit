ac7cece07b8239df828c86fee0a162ca6b8ed004
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-11-30 10:39:54 +0700

    create history

--- resources/views/frontend/user/history/index.blade.php
+++ resources/views/frontend/user/history/index.blade.php
@@ -12,13 +12,17 @@
       <div class="col row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
         @if ($histories->count() > 0)
           @foreach ($histories as $history)
-            <p>Order Date : {{ Carbon::parse($history->created_at)->format('d M Y') }}</p>
-            <p>Price : {{ Money::IDR($history->order->price, true) }}</p>
-            <p>Status : {{ $history->status->name }}</p>
-            <p>Paid At : {{ $history->issued_at ?? 'Unpaid' }}</p>
-            <a href={{ route('user.invoice.detail', $history->id) }}>See Detail</a>
-            <br>
-            <br>
+            <div class="col">
+              <div class="review-card">
+                <p class="review-text">
+                <p>Order Date : {{ Carbon::parse($history->created_at)->format('d M Y') }}</p>
+                </p>
+                <p class="review-text">Price : <strong>{{ Money::IDR($history->order->price, true) }}</strong></p>
+                <p class="review-text">Status : <strong>{{ $history->status->name }}</strong>
+                <p class="review-text">Paid At : {{ $history->issued_at ?? 'Unpaid' }}</p>
+                <a href={{ route('user.invoice.detail', $history->id) }} class="btn btn-primary">See Detail</a>
+              </div>
+            </div>
           @endforeach
         @else
           No History
