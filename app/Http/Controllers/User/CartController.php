a0cb7cbc1f0658b5a3dd69db359c53754c659a10
samuelsih <samuel.205025@mhs.its.ac.id>
2022-12-02 17:08:19 +0700

    chore: remove deps

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -3,13 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCartRequest;
-use App\Models\CoachDomain;
-use Carbon\Carbon;
-use Gloudemans\Shoppingcart\Facades\Cart;
-use Gloudemans\Shoppingcart\Exceptions\InvalidRowIDException;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
 
 class CartController extends Controller
 {
